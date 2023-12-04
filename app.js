dc550630b84a19cbb94d6016d9f399bffba31f66
csm <csm009177@gmail.com>
2023-12-04 18:34:08 +0900

    test typescript

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log('hello')
+// 예제 파일: hello.ts
+function sayHello(name) {
+    console.log("Hello, ".concat(name, "!"));
+}
+var myName = "YourName";
+sayHello(myName);
---
+++ app.ts
@@ -0,0 +1,7 @@
+// 예제 파일: hello.ts
+function sayHello(name: string): void {
+  console.log(`Hello, ${name}!`);
+}
+
+const myName: string = "YourName";
+sayHello(myName);

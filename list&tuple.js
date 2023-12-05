60b85dec78ff38b2a7a699d5056b9295e4bad4c7
csm <csm009177@gmail.com>
2023-12-05 13:05:41 +0900

    re install

---
+++ list&tuple.js
@@ -0,0 +1,4 @@
+var numbers = [1, 2, 3];
+var tuple = ["John", 25];
+console.log(numbers, tuple);
+//# sourceMappingURL=list&tuple.js.map
---
+++ list&tuple.ts
@@ -0,0 +1,4 @@
+let numbers: number[] = [1, 2, 3];
+let tuple: [string, number] = ["John", 25];
+
+console.log(numbers, tuple)

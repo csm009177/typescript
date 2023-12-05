626254aa67dbe0fc1e80dce4885af32532d4a61b
csm <csm009177@gmail.com>
2023-12-05 15:54:46 +0900

    test typescript-2-interface.ts

--- typescript-2-interface.ts
+++ typescript-2-interface.ts
@@ -39,4 +39,4 @@ function createComponent(element:string, props:Props, children?:string[]): strin
 //사용예시
 const myComponent = createComponent('div', {id:'example', style:{color:'red'}}, ['안녕하세요']);
 const root = document.getElementById('root');
-root?.innerHTML = myComponent;
+root.innerHTML = myComponent;

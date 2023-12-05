3afb7c4f69093f889a51008da9cd4fe23e56ecb1
csm <csm009177@gmail.com>
2023-12-05 17:42:43 +0900

    unionType.ts

---
+++ unionType.js
@@ -0,0 +1,4 @@
+var result = "first";
+var result2 = "second";
+var result3 = "third";
+console.log(result, result2, result3);
---
+++ unionType.ts
@@ -0,0 +1,6 @@
+type Status = "first" | "second" | "third";
+const result: Status = "first";
+const result2: Status = "second";
+const result3: Status = "third";
+
+console.log(result, result2, result3)
